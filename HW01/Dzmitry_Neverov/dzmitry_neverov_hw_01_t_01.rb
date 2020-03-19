2ad4c3ad056dfc7e7313ecfa8bf4c2ddec472e47
Neverov Dima <xomanob@mail.ru>
2020-03-19 22:15:35 +0300

    Robokop satisfied

--- HW01/Dzmitry_Neverov/dzmitry_neverov_hw_01_t_01.rb
+++ HW01/Dzmitry_Neverov/dzmitry_neverov_hw_01_t_01.rb
@@ -1,14 +1,13 @@
-h = gets.to_i
-m = gets.to_i
+hour = gets.to_i
+min = gets.to_i
 # this method turns the time into an angle between the watch strips
-def your_angel(h, m)
-  an = (m * 6 - (h * 60 + m) * 0.5).abs
+def your_angel(hour, min)
+  an = (min * 6 - (hour * 60 + min) * 0.5).abs
   if an > 180
     360 - an .abs
   else
-    an.abs
+    an .abs
   end
 end
 
-puts "#{your_angel(h, m)} degrees"
-
+puts "#{your_angel(hour, min)} degrees"
