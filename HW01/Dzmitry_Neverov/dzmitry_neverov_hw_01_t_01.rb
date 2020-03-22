87cbf8517951bd6e696a0ea860d4ac7c6d68ec6c
Neverov Dima <xomanob@mail.ru>
2020-03-22 13:11:17 +0300

    HW1.T1.Сhanged function name

--- HW01/Dzmitry_Neverov/dzmitry_neverov_hw_01_t_01.rb
+++ HW01/Dzmitry_Neverov/dzmitry_neverov_hw_01_t_01.rb
@@ -1,7 +1,7 @@
 hours = gets.to_i
 minutes = gets.to_i
 # this method turns the time into an angle between the watch strips
-def calc_angel(hours, minutes)
+def calc_angle(hours, minutes)
   angel = (minutes * 6 - (hours * 60 + minutes) * 0.5).abs
   if angel > 180
     360 - angel.abs
