9383078a41f975ab81ce94a9586358df59dcbd0c
Neverov Dima <xomanob@mail.ru>
2020-03-27 20:27:39 +0300

    Task2:script for check logs

---
+++ HW02/Dzmitry_Neverov/task2/logs_validator.rb
@@ -0,0 +1,27 @@
+class LogsValidator
+  attr_reader :logs_to_check
+
+  def initialize(logs_to_check)
+    @logs_to_check = logs_to_check
+  end
+
+  LOG_VALIDATE = %r{
+(?<ip>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}).{5}\[
+(?<date>\d{1,2}/\w{3}/\d{4}\:\d{,2}:\d{,2}:\d{,2}\s\+\d{4})\]\s"POST\s
+(?<url>/\w+/\d+/\w+)\s
+}x.freeze
+
+  def task_2
+    valid_logs = 0
+    logs_to_check.map do |line|
+      next unless line.match?(LOG_VALIDATE)
+
+      matches = line.match(LOG_VALIDATE)
+
+      puts "#{matches[:date]} FROM: #{matches[:ip]} TO: #{matches[:url].upcase}"
+      valid_logs += 1
+    end
+
+    puts if valid_logs.zero?
+  end
+end
