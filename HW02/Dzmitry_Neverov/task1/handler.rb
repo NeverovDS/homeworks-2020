1216f153b30bf2b6dfb20f8a75f88404abf6f308
NeverovDS <52862031+NeverovDS@users.noreply.github.com>
2020-03-28 13:55:00 +0300

    Update handler.rb
    Task1: method return only first error

--- HW02/Dzmitry_Neverov/task1/handler.rb
+++ HW02/Dzmitry_Neverov/task1/handler.rb
@@ -1,9 +1,10 @@
 logs = File.readlines("#{__dir__}/logs_hw_02_t_01.txt")
 
 def task_1(logs)
-  logs.select do |line|
+  arr = logs.select do |line|
     line.downcase =~ /.*error.*/
   end
+  puts("First error #{arr.first}")
 end
 
 puts task_1(logs)
