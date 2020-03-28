fdec3618d7a08edf6f298daf26a2347ff7415ec2
Neverov Dima <xomanob@mail.ru>
2020-03-28 14:13:59 +0300

    Task3:constant under the class name

--- HW02/Dzmitry_Neverov/task3/line_search.rb
+++ HW02/Dzmitry_Neverov/task3/line_search.rb
@@ -1,11 +1,10 @@
 class LineSearcher
+  NEED_LOG = /.*Calling core with action.*/.freeze
   attr_reader :some_lines
 
   def initialize(some_lines)
     @some_lines = some_lines
   end
-
-  NEED_LOG = /.*Calling core with action.*/.freeze
 
   def find_lines
     some_lines.select { |line| line.match?(NEED_LOG) }
