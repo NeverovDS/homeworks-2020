5a8a604036672ba2a9eb422e4f4d999d6ee6fe95
Neverov Dima <xomanob@mail.ru>
2020-03-29 16:44:52 +0300

    Tas3:add method print ,then output and logic func dont combine.In def calc_differ 'if' more correct

--- HW02/Dzmitry_Neverov/task3/time_calculator.rb
+++ HW02/Dzmitry_Neverov/task3/time_calculator.rb
@@ -18,28 +18,28 @@ class TimeIntervalCalculator
     arr[1..arr.size].map!.with_index(1) do |time, i|
       arr_for_difference << (time - arr[i - 1]).abs
     end
-    puts("arr_for_difference #{arr_for_difference}")
-  end
-
-  def calc_differ(arr)
-    if arr.length > 2
-      check_length(arr)
-    else
-      puts((arr[1] - arr[0]).to_s)
-    end
-  end
-
-  def time_one?(arr_for_times)
-    arr_for_times.length == 1
+    print(arr_for_difference)
   end
 
   def task_3
     arr_for_times = []
     search_time.each { |time| arr_for_times << Time.parse(time) }
-    if time_one?(arr_for_times).trust
-      puts(arr_for_times)
+    if arr_for_times.length == 1
+      print(arr_for_times)
     else
       calc_differ(arr_for_times)
     end
   end
+end
+def print(arr)
+  puts(arr)
+end
+private
+
+def calc_differ(arr)
+if arr.length > 2
+  check_length(arr)
+else
+  print((arr[1] - arr[0]).to_s)
+end
 end
