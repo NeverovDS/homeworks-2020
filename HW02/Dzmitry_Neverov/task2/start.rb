366a995deaa927c98bef10ab036ebbd14a9b9ec5
Neverov Dima <xomanob@mail.ru>
2020-04-01 21:31:07 +0300

    delete hw2 in branch for hw3

--- HW02/Dzmitry_Neverov/task2/start.rb
+++
@@ -1,5 +0,0 @@
-require_relative 'logs_reader.rb'
-require_relative 'logs_validator.rb'
-
-logs = LogsReader.new(ARGV[0]).read_file
-LogsValidator.new(logs).task_2
--- HW02/Dzmitry_Neverov/task4/h2_t4.rb
+++
@@ -1,9 +0,0 @@
-def task_4(some_str)
-  letters = some_str.scan(/[a-zA-z]/)
-  digits = some_str.scan(/[0-9]/)
-  puts "letters: #{letters.length}, digits: #{digits.length}"
-end
-
-task_4('dxchvjbk')
-task_4('wweths!')
-task_4('aywrjnsdbgfva')
