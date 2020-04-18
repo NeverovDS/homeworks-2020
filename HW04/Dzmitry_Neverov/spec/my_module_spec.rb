17d43fee8b1127edf470b876571c037b7b1eaee5
Neverov Dima <xomanob@mail.ru>
2020-04-18 14:15:43 +0300

    rubocop

--- HW04/Dzmitry_Neverov/spec/my_module_spec.rb
+++ HW04/Dzmitry_Neverov/spec/my_module_spec.rb
@@ -1,17 +1,17 @@
 require 'rspec'
 require 'my_module.rb'
 
-describe 'my_each' do
+describe '#my_each' do
   let(:a) { Array.new([1, 2, 3, 4, 5]) }
   let(:result) { [] }
 
   context 'when has a block' do
-
     it 'yields each element to the block' do
       expect do |block|
         a.my_each(&block)
       end.to yield_successive_args(1, 2, 3, 4, 5)
     end
+
     it 'will return a calculated array' do
       result = []
       a.my_each { |x| result << x + 3 }
@@ -24,16 +24,16 @@ describe 'my_each' do
   end
 
   context 'when has not a block' do
-
     it 'returns the Enumerator' do
       expect(a.my_each).to be_an_instance_of(Enumerator)
     end
   end
 end
-describe 'my_map' do
-  let(:a) { Array.new([1, 2, 3, 4, 5]) }
-  context 'when has a block' do
 
+describe '#my_map' do
+  let(:a) { Array.new([1, 2, 3, 4, 5]) }
+
+  context 'when has a block' do
     it 'will return a calculated array' do
       expect(a.my_map { |x| x + 3 }).to eq([4, 5, 6, 7, 8])
     end
@@ -42,39 +42,41 @@ describe 'my_map' do
       expect(a.my_map { |x| x + 3 }).to eq(a.map { |x| x + 3 })
     end
   end
-  context 'when has not a block' do
 
+  context 'when has not a block' do
     it 'returns the Enumerator' do
       expect(a.my_map).to be_an_instance_of(Enumerator)
     end
   end
-  context 'when empty array' do
 
+  context 'when empty array' do
     let(:a) { Array.new([]) }
     it 'returns an empty array' do
       expect(a.my_map { |x| x + 3 }).to eq([])
     end
   end
 end
-describe 'my_select' do
-  let(:a) { Array.new([1, 2, 3, 4, 5]) }
-  context 'when has a block' do
 
+describe '#my_select' do
+  let(:a) { Array.new([1, 2, 3, 4, 5]) }
+
+  context 'when has a block' do
     it 'returns selected array ' do
       expect(a.my_select { |x| x < 3 }).to eq([1, 2])
     end
+
     it 'the result is the same as default method select' do
       expect(a.my_select { |x| x < 3 }).to eq(a.select { |x| x < 3 })
     end
   end
-  context 'when has not a block' do
 
+  context 'when has not a block' do
     it 'returns the Enumerator' do
       expect(a.my_select).to be_an_instance_of(Enumerator)
     end
   end
-  context 'when empty array' do
 
+  context 'when empty array' do
     let(:a) { Array.new([]) }
     it 'returns an empty array' do
       expect(a.my_select { |x| x < 3 }).to eq([])
