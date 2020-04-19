d793eb6c9099a65a0e997759034a2d3119cb399b
Neverov Dima <xomanob@mail.ru>
2020-04-19 20:52:24 +0300

    all in 1 file

---
+++ HW04/Dzmitry_Neverov/spec/my_module_all_arr_spec.rb
@@ -0,0 +1,90 @@
+require_relative 'my_module.rb'
+RSpec.describe Array do
+  let(:test_arr) { described_class.new([1, 2, 3, 4, 5]) }
+  let(:empty_arr) { described_class.new([]) }
+
+  describe '#my_map' do
+    context 'when has a block' do
+      it 'will return a calculated array' do
+        expect(test_arr.my_map { |x| x + 3 }).to eq([4, 5, 6, 7, 8])
+      end
+
+      it 'the result is the same as default method map' do
+        expect(test_arr.my_map { |x| x + 3 }).to eq(test_arr.map { |x| x + 3 })
+      end
+    end
+
+    context 'when has not a block' do
+      it 'returns the Enumerator' do
+        expect(test_arr.my_map).to be_an_instance_of(Enumerator)
+      end
+    end
+
+    context 'when empty array' do
+      it 'returns an empty array' do
+        expect(empty_arr.my_map { |x| x + 3 }).to eq([])
+      end
+    end
+  end
+end
+
+RSpec.describe Array do
+  let(:test_array) { described_class.new([1, 2, 3, 4, 5]) }
+
+  describe '#my_each' do
+    context 'when has a block' do
+      it 'yields each element to the block' do
+        expect do |block|
+          test_array.my_each(&block)
+        end.to yield_successive_args(1, 2, 3, 4, 5)
+      end
+
+      it 'will return a calculated array' do
+        result = []
+        test_array.my_each { |x| result << x + 3 }
+        expect(result).to eq([4, 5, 6, 7, 8])
+      end
+
+      it 'returns the original array' do
+        expect(test_array.my_each {}).to eq([1, 2, 3, 4, 5])
+      end
+    end
+
+    context 'when empty array ' do
+      let(:empty_arr) { described_class.new([]) }
+      it 'returns an empty array' do
+        expect(empty_arr.my_each { |x| x + 3 }).to eq([])
+      end
+    end
+  end
+end
+
+RSpec.describe Array do
+  let(:test) { described_class.new([1, 2, 3, 4, 5]) }
+
+  describe '#my_select' do
+    context 'when has a block' do
+      it 'returns selected array ' do
+        expect(test.my_select { |x| x < 3 }).to eq([1, 2])
+      end
+
+      it 'the result is the same as default method select' do
+        expect(test.my_select { |x| x < 3 }).to eq(test.select { |x| x < 3 })
+      end
+    end
+
+    context 'when has not a block' do
+      it 'returns the Enumerator' do
+        expect(test.my_select).to be_an_instance_of(Enumerator)
+      end
+    end
+
+    context 'when empty array' do
+      let(:test_empty) { described_class.new([]) }
+
+      it 'returns an empty array' do
+        expect(test_empty.my_select { |x| x < 3 }).to eq([])
+      end
+    end
+  end
+end
